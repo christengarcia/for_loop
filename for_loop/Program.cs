24bc86b80357a3b723dc5e083b599989aa0b9cdb
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 21:14:29 +1100

    Used for loop and Length property of the array to loop through the array.

--- for_loop/Program.cs
+++ for_loop/Program.cs
@@ -6,9 +6,16 @@ namespace for_loop
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            /*for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
+            }*/
+
+            int[] myNumbers = { 3, 8, 2, 37, 21, 55, 34, 88 };
+
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                Console.WriteLine(myNumbers[i]);
             }
         }
     }
